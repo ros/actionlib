1aaa5ddf455768da78b50d215c1b0c59f5fabefb
jfaust <devnull@localhost>
2009-09-18 01:20:04 +0000

    Remove ros/node.h inclusions

--- actionlib_tutorials/simple_action_clients/fibonacci_client.cpp
+++ actionlib_tutorials/simple_action_clients/fibonacci_client.cpp
@@ -35,7 +35,7 @@
  *********************************************************************/
 
 /* Author: Melonee Wise */
-#include <ros/node.h>
+#include <ros/ros.h>
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
 #include <actionlib_tutorials/FibonacciAction.h>
