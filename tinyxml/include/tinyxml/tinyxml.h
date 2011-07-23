bf3885463b7261bcc5778dfb4705b6b044f09475
Tully Foote <tully.foote@gmail.com>
2011-07-22 20:40:51 -0700

    adding tinyxml metapackage #5080

---
+++ tinyxml/include/tinyxml/tinyxml.h
@@ -0,0 +1,8 @@
+#ifndef _ROS_TINYXML_TINYXML_H_
+#define _ROS_TINYXML_TINYXML_H_
+
+#warning "ROS copy of TINYXML deprecated. You should include <tinyxml.h> in your package"
+
+#include <tinyxml.h>
+
+#endif // guard
