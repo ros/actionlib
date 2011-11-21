39b80868f0b1a7581f27afb43299d514c3a78297
Wim Meeussen <wim@willowgarage.com>
2011-11-21 11:29:58 -0800

    remove deprecated tinyxml and yamlcpp

--- tinyxml/include/tinyxml/tinyxml.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ROS_TINYXML_TINYXML_H_
-#define _ROS_TINYXML_TINYXML_H_
-
-#warning "ROS copy of TINYXML deprecated. You should include <tinyxml.h> in your package"
-
-#include <tinyxml.h>
-
-#endif // guard
