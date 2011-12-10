bf56e38492584d3d67c5f6230c3fa5a2b04e0e84
Morten Kjaergaard <devnull@localhost>
2011-12-09 16:45:02 -0800

    Updates to catkin support

--- catkin.cmake
+++ catkin.cmake
@@ -2,10 +2,14 @@ cmake_minimum_required(VERSION 2.8)
 project(actionlib)
 
 find_package(catkin)
+find_package(ROS COMPONENTS
+  rostime roscpp_serialization roscpp_traits cpp_common # serialization stuff
+  roscpp rosconsole) #roscpp stuff
 find_package(actionlib_msgs)
 find_package(PythonLibs)
 
 include_directories(include)
+include_directories(${ROS_INCLUDE_DIRS})
 
 add_library(actionlib src/connection_monitor.cpp
                       src/goal_id_generator.cpp)
