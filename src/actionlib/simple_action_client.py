ros/actionlib

commit da39083c513066e0d3d05346f3b1be9f9f90268d

Python code cleanup (#43)
* Cleaned up semicolons, indentation, spaces.

* Removed unused local var after further confirmation of no risk of side effects.

src/actionlib/simple_action_client.py
@@ -27,18 +27,18 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 # Author: Stuart Glaser
-
-import threading
-import time
 import rospy
-from rospy import Header
+import threading
+
 from actionlib_msgs.msg import *
 from actionlib.action_client import ActionClient, CommState, get_name_of_constant
+
 
 class SimpleGoalState:
     PENDING = 0
     ACTIVE = 1
     DONE = 2
+
 SimpleGoalState.to_string = classmethod(get_name_of_constant)
 
 
@@ -62,9 +62,8 @@ class SimpleActionClient:
     ## timeout is interpreted as an infinite timeout.
     ##
     ## @return True if the server connected in the allocated time. False on timeout
-    def wait_for_server(self, timeout = rospy.Duration()):
+    def wait_for_server(self, timeout=rospy.Duration()):
         return self.action_client.wait_for_server(timeout)
-
 
     ## @brief Sends a goal to the ActionServer, and also registers callbacks
     ##
@@ -80,7 +79,7 @@ class SimpleActionClient:
     ##
     ## @param feedback_cb Callback that gets called whenever feedback
     ## for this goal is received.  Takes one parameter: the feedback.
-    def send_goal(self, goal, done_cb = None, active_cb = None, feedback_cb = None):
+    def send_goal(self, goal, done_cb=None, active_cb=None, feedback_cb=None):
         # destroys the old goal handle
         self.stop_tracking_goal()
 
@@ -90,7 +89,6 @@ class SimpleActionClient:
 
         self.simple_state = SimpleGoalState.PENDING
         self.gh = self.action_client.send_goal(goal, self._handle_transition, self._handle_feedback)
-
 
     ## @brief Sends a goal to the ActionServer, waits for the goal to complete, and preempts goal is necessary
     ##
@@ -107,23 +105,23 @@ class SimpleActionClient:
     ## @param preempt_timeout The time to wait for preemption to complete
     ##
     ## @return The goal's state.
-    def send_goal_and_wait(self, goal, execute_timeout = rospy.Duration(), preempt_timeout = rospy.Duration()):
+    def send_goal_and_wait(self, goal, execute_timeout=rospy.Duration(), preempt_timeout=rospy.Duration()):
         self.send_goal(goal)
         if not self.wait_for_result(execute_timeout):
             # preempt action
             rospy.logdebug("Canceling goal")
             self.cancel_goal()
             if self.wait_for_result(preempt_timeout):
-                rospy.logdebug("Preempt finished within specified preempt_timeout [%.2f]", preempt_timeout.to_sec());
+                rospy.logdebug("Preempt finished within specified preempt_timeout [%.2f]", preempt_timeout.to_sec())
             else:
-                rospy.logdebug("Preempt didn't finish specified preempt_timeout [%.2f]", preempt_timeout.to_sec());
+                rospy.logdebug("Preempt didn't finish specified preempt_timeout [%.2f]", preempt_timeout.to_sec())
         return self.get_state()
 
 
     ## @brief Blocks until this goal transitions to done
     ## @param timeout Max time to block before returning. A zero timeout is interpreted as an infinite timeout.
     ## @return True if the goal finished. False if the goal didn't finish within the allocated timeout
-    def wait_for_result(self, timeout = rospy.Duration()):
+    def wait_for_result(self, timeout=rospy.Duration()):
         if not self.gh:
             rospy.logerr("Called wait_for_goal_to_finish when no goal exists")
             return False
@@ -146,7 +144,6 @@ class SimpleActionClient:
 
         return self.simple_state == SimpleGoalState.DONE
 
-
     ## @brief Gets the Result of the current goal
     def get_result(self):
         if not self.gh:
@@ -154,7 +151,6 @@ class SimpleActionClient:
             return None
 
         return self.gh.get_result()
-
 
     ## @brief Get the state information for this goal
     ##
@@ -175,7 +171,6 @@ class SimpleActionClient:
             status = GoalStatus.ACTIVE
 
         return status
-
 
     ## @brief Returns the current status text of the goal.
     ##
@@ -203,7 +198,7 @@ class SimpleActionClient:
 
     ## @brief Cancels all goals prior to a given timestamp
     ##
-    ## This preempts all goals running on the action server for which the 
+    ## This preempts all goals running on the action server for which the
     ## time stamp is earlier than the specified time stamp
     ## this message is serviced by the ActionServer.
 
@@ -259,17 +254,15 @@ class SimpleActionClient:
 
     def _handle_feedback(self, gh, feedback):
         if not self.gh:
-            rospy.logerr("Got a feedback callback when we're not tracking a goal. (id: %s)" % \
-                             gh.comm_state_machine.action_goal.goal_id.id)
+            rospy.logerr("Got a feedback callback when we're not tracking a goal. (id: %s)" %
+                         gh.comm_state_machine.action_goal.goal_id.id)
             return
         if gh != self.gh:
-            rospy.logerr("Got a feedback callback on a goal handle that we're not tracking. %s vs %s" % \
-                             (self.gh.comm_state_machine.action_goal.goal_id.id,
-                              gh.comm_state_machine.action_goal.goal_id.id))
+            rospy.logerr("Got a feedback callback on a goal handle that we're not tracking. %s vs %s" %
+                         (self.gh.comm_state_machine.action_goal.goal_id.id, gh.comm_state_machine.action_goal.goal_id.id))
             return
         if self.feedback_cb:
             self.feedback_cb(feedback)
-
 
     def _set_simple_state(self, state):
         self.simple_state = state

src/actionlib/__init__.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2009, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -29,6 +29,3 @@ from actionlib.action_client import *
 from actionlib.simple_action_client import *
 from actionlib.action_server import *
 from actionlib.simple_action_server import *
-
-
-

src/actionlib/action_client.py
@@ -60,6 +60,7 @@ from actionlib.exceptions import *
 
 g_goal_id = 1
 
+
 def get_name_of_constant(C, n):
     for k, v in C.__dict__.items():
         if type(v) is int and v == n:
@@ -78,6 +79,7 @@ class CommState(object):
     DONE = 7
     LOST = 8
 
+
 class TerminalState(object):
     RECALLED = GoalStatus.RECALLED
     REJECTED = GoalStatus.REJECTED
@@ -91,11 +93,13 @@ GoalStatus.to_string = classmethod(get_name_of_constant)
 CommState.to_string = classmethod(get_name_of_constant)
 TerminalState.to_string = classmethod(get_name_of_constant)
 
+
 def _find_status_by_goal_id(status_array, id):
     for s in status_array.status_list:
         if s.goal_id.id == id:
             return s
     return None
+
 
 ## @brief Client side handle to monitor goal progress.
 ##
@@ -111,7 +115,7 @@ class ClientGoalHandle:
     def __init__(self, comm_state_machine):
         self.comm_state_machine = comm_state_machine
 
-        #print "GH created.  id = %.3f" % self.comm_state_machine.action_goal.goal_id.stamp.to_sec()
+        # print "GH created.  id = %.3f" % self.comm_state_machine.action_goal.goal_id.stamp.to_sec()
 
     ## @brief True iff the two ClientGoalHandle's are tracking the same goal
     def __eq__(self, o):
@@ -134,8 +138,8 @@ class ClientGoalHandle:
     ## Also transitions the client state to WAITING_FOR_CANCEL_ACK
     def cancel(self):
         with self.comm_state_machine.mutex:
-            cancel_msg = GoalID(stamp = rospy.Time(0),
-                                id = self.comm_state_machine.action_goal.goal_id.id)
+            cancel_msg = GoalID(stamp=rospy.Time(0),
+                                id=self.comm_state_machine.action_goal.goal_id.id)
             self.comm_state_machine.send_cancel_fn(cancel_msg)
             self.comm_state_machine.transition_to(CommState.WAITING_FOR_CANCEL_ACK)
 
@@ -200,7 +204,7 @@ class ClientGoalHandle:
         with self.comm_state_machine.mutex:
             if self.comm_state_machine.state != CommState.DONE:
                 rospy.logwarn("Asking for the terminal state when we're in [%s]",
-                             CommState.to_string(self.comm_state_machine.state))
+                              CommState.to_string(self.comm_state_machine.state))
 
             goal_status = self.comm_state_machine.latest_goal_status.status
             if goal_status in [GoalStatus.PREEMPTED, GoalStatus.SUCCEEDED,
@@ -210,8 +214,6 @@ class ClientGoalHandle:
 
             rospy.logerr("Asking for a terminal state, but the goal status is %d", goal_status)
             return GoalStatus.LOST
-
-
 
 
 NO_TRANSITION = -1
@@ -226,7 +228,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  (CommState.ACTIVE, CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.SUCCEEDED:  (CommState.ACTIVE, CommState.WAITING_FOR_RESULT),
         GoalStatus.ABORTED:    (CommState.ACTIVE, CommState.WAITING_FOR_RESULT),
-        GoalStatus.PREEMPTING: (CommState.ACTIVE, CommState.PREEMPTING) },
+        GoalStatus.PREEMPTING: (CommState.ACTIVE, CommState.PREEMPTING)},
     CommState.PENDING: {
         GoalStatus.PENDING:    NO_TRANSITION,
         GoalStatus.ACTIVE:     CommState.ACTIVE,
@@ -236,7 +238,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  (CommState.ACTIVE, CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.SUCCEEDED:  (CommState.ACTIVE, CommState.WAITING_FOR_RESULT),
         GoalStatus.ABORTED:    (CommState.ACTIVE, CommState.WAITING_FOR_RESULT),
-        GoalStatus.PREEMPTING: (CommState.ACTIVE, CommState.PREEMPTING) },
+        GoalStatus.PREEMPTING: (CommState.ACTIVE, CommState.PREEMPTING)},
     CommState.ACTIVE: {
         GoalStatus.PENDING:    INVALID_TRANSITION,
         GoalStatus.ACTIVE:     NO_TRANSITION,
@@ -246,7 +248,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.SUCCEEDED:  CommState.WAITING_FOR_RESULT,
         GoalStatus.ABORTED:    CommState.WAITING_FOR_RESULT,
-        GoalStatus.PREEMPTING: CommState.PREEMPTING },
+        GoalStatus.PREEMPTING: CommState.PREEMPTING},
     CommState.WAITING_FOR_RESULT: {
         GoalStatus.PENDING:    INVALID_TRANSITION,
         GoalStatus.ACTIVE:     NO_TRANSITION,
@@ -256,7 +258,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  NO_TRANSITION,
         GoalStatus.SUCCEEDED:  NO_TRANSITION,
         GoalStatus.ABORTED:    NO_TRANSITION,
-        GoalStatus.PREEMPTING: INVALID_TRANSITION },
+        GoalStatus.PREEMPTING: INVALID_TRANSITION},
     CommState.WAITING_FOR_CANCEL_ACK: {
         GoalStatus.PENDING:    NO_TRANSITION,
         GoalStatus.ACTIVE:     NO_TRANSITION,
@@ -266,7 +268,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.SUCCEEDED:  (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.ABORTED:    (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
-        GoalStatus.PREEMPTING: CommState.PREEMPTING },
+        GoalStatus.PREEMPTING: CommState.PREEMPTING},
     CommState.RECALLING: {
         GoalStatus.PENDING:    INVALID_TRANSITION,
         GoalStatus.ACTIVE:     INVALID_TRANSITION,
@@ -276,7 +278,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.SUCCEEDED:  (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
         GoalStatus.ABORTED:    (CommState.PREEMPTING, CommState.WAITING_FOR_RESULT),
-        GoalStatus.PREEMPTING: CommState.PREEMPTING },
+        GoalStatus.PREEMPTING: CommState.PREEMPTING},
     CommState.PREEMPTING: {
         GoalStatus.PENDING:    INVALID_TRANSITION,
         GoalStatus.ACTIVE:     INVALID_TRANSITION,
@@ -286,7 +288,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  CommState.WAITING_FOR_RESULT,
         GoalStatus.SUCCEEDED:  CommState.WAITING_FOR_RESULT,
         GoalStatus.ABORTED:    CommState.WAITING_FOR_RESULT,
-        GoalStatus.PREEMPTING: NO_TRANSITION },
+        GoalStatus.PREEMPTING: NO_TRANSITION},
     CommState.DONE: {
         GoalStatus.PENDING:    INVALID_TRANSITION,
         GoalStatus.ACTIVE:     INVALID_TRANSITION,
@@ -296,8 +298,7 @@ _transitions = {
         GoalStatus.PREEMPTED:  NO_TRANSITION,
         GoalStatus.SUCCEEDED:  NO_TRANSITION,
         GoalStatus.ABORTED:    NO_TRANSITION,
-        GoalStatus.PREEMPTING: INVALID_TRANSITION } }
-
+        GoalStatus.PREEMPTING: INVALID_TRANSITION}}
 
 
 class CommStateMachine:
@@ -310,7 +311,7 @@ class CommStateMachine:
 
         self.state = CommState.WAITING_FOR_GOAL_ACK
         self.mutex = threading.RLock()
-        self.latest_goal_status = GoalStatus(status = GoalStatus.PENDING)
+        self.latest_goal_status = GoalStatus(status=GoalStatus.PENDING)
         self.latest_result = None
 
     def __eq__(self, o):
@@ -411,7 +412,7 @@ class CommStateMachine:
         if self.action_goal.goal_id.id != action_feedback.status.goal_id.id:
             return
 
-        #with self.mutex:
+        # with self.mutex:
         if self.feedback_cb and self.state != CommState.DONE:
             self.feedback_cb(ClientGoalHandle(self), action_feedback.feedback)
 
@@ -439,21 +440,21 @@ class GoalManager:
         global g_goal_id
         id, g_goal_id = g_goal_id, g_goal_id + 1
         now = rospy.Time.now()
-        return GoalID(id = "%s-%i-%.3f" % \
-                          (rospy.get_caller_id(), id, now.to_sec()), stamp = now)
+        return GoalID(id="%s-%i-%.3f" % (rospy.get_caller_id(), id, now.to_sec()), stamp=now)
 
     def register_send_goal_fn(self, fn):
         self.send_goal_fn = fn
+
     def register_cancel_fn(self, fn):
         self.cancel_fn = fn
 
     ## Sends off a goal and starts tracking its status.
     ##
     ## @return ClientGoalHandle for the sent goal.
-    def init_goal(self, goal, transition_cb = None, feedback_cb = None):
-        action_goal = self.ActionGoal(header = Header(),
-                                      goal_id = self._generate_id(),
-                                      goal = goal)
+    def init_goal(self, goal, transition_cb=None, feedback_cb=None):
+        action_goal = self.ActionGoal(header=Header(),
+                                      goal_id=self._generate_id(),
+                                      goal=goal)
         action_goal.header.stamp = rospy.get_rostime()
 
         csm = CommStateMachine(action_goal, transition_cb, feedback_cb,
@@ -466,7 +467,6 @@ class GoalManager:
 
         return ClientGoalHandle(csm)
 
-
     # Pulls out the statuses that are still live (creating strong
     # references to them)
     def _get_live_statuses(self):
@@ -475,12 +475,10 @@ class GoalManager:
             live_statuses = filter(lambda x: x, live_statuses)
             return live_statuses
 
-
     ## Updates the statuses of all goals from the information in status_array.
     ##
     ## @param status_array (\c actionlib_msgs/GoalStatusArray)
     def update_statuses(self, status_array):
-        live_statuses = []
 
         with self.list_mutex:
             # Garbage collects dead status objects
@@ -489,7 +487,6 @@ class GoalManager:
         for status in self._get_live_statuses():
             status.update_status(status_array)
 
-
     def update_results(self, action_result):
         for status in self._get_live_statuses():
             status.update_result(action_result)
@@ -497,6 +494,7 @@ class GoalManager:
     def update_feedbacks(self, action_feedback):
         for status in self._get_live_statuses():
             status.update_feedback(action_feedback)
+
 
 class ActionClient:
     ## @brief Constructs an ActionClient and opens connections to an ActionServer.
@@ -545,7 +543,7 @@ class ActionClient:
     ## message.
     ##
     ## @return ClientGoalHandle for the sent goal.
-    def send_goal(self, goal, transition_cb = None, feedback_cb = None):
+    def send_goal(self, goal, transition_cb=None, feedback_cb=None):
         return self.manager.init_goal(goal, transition_cb, feedback_cb)
 
     ## @brief Cancels all goals currently running on the action server.
@@ -553,8 +551,7 @@ class ActionClient:
     ## Preempts all goals running on the action server at the point
     ## that the cancel message is serviced by the action server.
     def cancel_all_goals(self):
-        cancel_msg = GoalID(stamp = rospy.Time.from_sec(0.0),
-                            id = "")
+        cancel_msg = GoalID(stamp=rospy.Time.from_sec(0.0), id="")
         self.pub_cancel.publish(cancel_msg)
 
     ## @brief Cancels all goals prior to a given timestamp
@@ -564,12 +561,12 @@ class ActionClient:
     ## this message is serviced by the ActionServer.
 
     def cancel_goals_at_and_before_time(self, time):
-        cancel_msg = GoalID(stamp = time, id = "")
+        cancel_msg = GoalID(stamp=time, id="")
         self.pub_cancel.publish(cancel_msg)
 
 
     ## @brief [Deprecated] Use wait_for_server
-    def wait_for_action_server_to_start(self, timeout = rospy.Duration(0.0)):
+    def wait_for_action_server_to_start(self, timeout=rospy.Duration(0.0)):
         return self.wait_for_server(timeout)
 
     ## @brief Waits for the ActionServer to connect to this client
@@ -577,7 +574,7 @@ class ActionClient:
     ## Often, it can take a second for the action server & client to negotiate
     ## a connection, thus, risking the first few goals to be dropped. This call lets
     ## the user wait until the network connection to the server is negotiated
-    def wait_for_server(self, timeout = rospy.Duration(0.0)):
+    def wait_for_server(self, timeout=rospy.Duration(0.0)):
         started = False
         timeout_time = rospy.get_rostime() + timeout
         while not rospy.is_shutdown():
@@ -586,9 +583,9 @@ class ActionClient:
 
                 if self.pub_goal.impl.has_connection(server_id) and \
                         self.pub_cancel.impl.has_connection(server_id):
-                    #We'll also check that all of the subscribers have at least
-                    #one publisher, this isn't a perfect check, but without
-                    #publisher callbacks... it'll have to do
+                    # We'll also check that all of the subscribers have at least
+                    # one publisher, this isn't a perfect check, but without
+                    # publisher callbacks... it'll have to do
                     status_num_pubs = 0
                     for stat in self.status_sub.impl.get_stats()[1]:
                         if stat[4]:

src/actionlib/exceptions.py
@@ -28,4 +28,6 @@
 
 # Author: Jonathan Bohren
 
-class ActionException(Exception): pass
+
+class ActionException(Exception):
+    pass

src/actionlib/goal_id_generator.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
 # Copyright (c) 2009, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -13,7 +13,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -26,26 +26,22 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Author: Alexander Sorokin. 
+# Author: Alexander Sorokin.
 # Based on C++ goal_id_generator.h/cpp
-
 import rospy
-
-import sys
 
 from actionlib_msgs.msg import GoalID
 import threading
 
 global s_goalcount_lock
 global s_goalcount
-s_goalcount_lock = threading.Lock();
+s_goalcount_lock = threading.Lock()
 s_goalcount = 0
 
 
 class GoalIDGenerator:
 
-
-    def __init__(self,name=None):
+    def __init__(self, name=None):
         """
         * Create a generator that prepends the fully qualified node name to the Goal ID
         * \param name Unique name to prepend to the goal id. This will
@@ -54,33 +50,30 @@ class GoalIDGenerator:
         if name is not None:
             self.set_name(name)
         else:
-            self.set_name(rospy.get_name());
+            self.set_name(rospy.get_name())
 
-
-    def set_name(self,name):
+    def set_name(self, name):
         """
         * \param name Set the name to prepend to the goal id. This will
         *             generally be a fully qualified node name.
         """
-        self.name=name;
-
-      
+        self.name = name
 
     def generate_ID(self):
         """
         * \brief Generates a unique ID
         * \return A unique GoalID for this action
         """
-        id = GoalID();
-        cur_time = rospy.Time.now();
-        ss = self.name +  "-";
+        id = GoalID()
+        cur_time = rospy.Time.now()
+        ss = self.name + "-"
         global s_goalcount_lock
         global s_goalcount
         with s_goalcount_lock:
             s_goalcount += 1
-            ss += str(s_goalcount) + "-";
-        ss +=  str(cur_time.secs) + "." + str(cur_time.nsecs);
+            ss += str(s_goalcount) + "-"
+        ss += str(cur_time.secs) + "." + str(cur_time.nsecs)
 
-        id.id = ss;
-        id.stamp = cur_time;
-        return id;
+        id.id = ss
+        id.stamp = cur_time
+        return id

src/actionlib/handle_tracker_deleter.py
@@ -1,9 +1,9 @@
 # Copyright (c) 2009, Willow Garage, Inc.
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 #       notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above copyright
@@ -12,7 +12,7 @@
 #     * Neither the name of the Willow Garage, Inc. nor the names of its
 #       contributors may be used to endorse or promote products derived from
 #       this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -25,10 +25,10 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-# Author: Alexander Sorokin. 
+# Author: Alexander Sorokin.
 # Based on C++ goal_id_generator.h/cpp
-
 import rospy
+
 
 class HandleTrackerDeleter:
     """
@@ -42,10 +42,10 @@ class HandleTrackerDeleter:
         """
         @brief create deleter
         """
-        self.action_server = action_server;
-        self.status_tracker = status_tracker;
+        self.action_server = action_server
+        self.status_tracker = status_tracker
 
-    def __call__(self,ptr):
+    def __call__(self, ptr):
         if self.action_server:
             with self.action_server.lock:
-                self.status_tracker.handle_destruction_time = rospy.Time.now();
+                self.status_tracker.handle_destruction_time = rospy.Time.now()

commit bbe2b658bb7b7503fbd5accaadc69e32c7685686

Remove misleading error log
This was introduced in https://github.com/ros/actionlib/pull/43.

It is not actually correct - you can feasibly get feedback here before a new goal is confirmed. See `send_goal()`....

```
    def send_goal(self, goal, done_cb=None, active_cb=None, feedback_cb=None):
        # destroys the old goal handle
        self.stop_tracking_goal()

        ...

        self.gh = self.action_client.send_goal(goal, self._handle_transition, self._handle_feedback)
```
and of course it will take more time on top of this for the server to actually process the incoming goal and confirm it. Meantime, it may have sent us feedback messages.

src/actionlib/simple_action_client.py
@@ -254,8 +254,9 @@ class SimpleActionClient:
 
     def _handle_feedback(self, gh, feedback):
         if not self.gh:
-            rospy.logerr("Got a feedback callback when we're not tracking a goal. (id: %s)" %
-                         gh.comm_state_machine.action_goal.goal_id.id)
+            # this is not actually an error - there can be a small window in which old feedback
+            # can be received between the time this variable is reset and a new goal is
+            # sent and confirmed
             return
         if gh != self.gh:
             rospy.logerr("Got a feedback callback on a goal handle that we're not tracking. %s vs %s" %