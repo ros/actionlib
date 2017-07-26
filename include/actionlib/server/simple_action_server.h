687bd6559d912ae48140f8d0aebfc0516e102240
Mikael Arguedas <mikael@osrfoundation.org>
2017-07-26 10:41:44 -0700

    forgot to update indent when rebasing

--- include/actionlib/server/simple_action_server.h
+++ include/actionlib/server/simple_action_server.h
@@ -70,8 +70,8 @@ public:
    * @brief  Constructor for a SimpleActionServer
    * @param name A name for the action server
    * @param execute_callback Optional callback that gets called in a separate thread whenever
-   *                   a new goal is received, allowing users to have blocking callbacks.
-   *                   Adding an execute callback also deactivates the goalCallback.
+   *                         a new goal is received, allowing users to have blocking callbacks.
+   *                         Adding an execute callback also deactivates the goalCallback.
    * @param  auto_start A boolean value that tells the ActionServer wheteher or not to start publishing as soon as it comes up. THIS SHOULD ALWAYS BE SET TO FALSE TO AVOID RACE CONDITIONS and start() should be called after construction of the server.
    */
   SimpleActionServer(std::string name, ExecuteCallback execute_callback, bool auto_start);
@@ -87,8 +87,8 @@ public:
    * @brief  DEPRECATED: Constructor for a SimpleActionServer
    * @param name A name for the action server
    * @param execute_callback Optional callback that gets called in a separate thread whenever
-   *                   a new goal is received, allowing users to have blocking callbacks.
-   *                   Adding an execute callback also deactivates the goalCallback.
+   *                         a new goal is received, allowing users to have blocking callbacks.
+   *                         Adding an execute callback also deactivates the goalCallback.
    */
   ROS_DEPRECATED SimpleActionServer(std::string name, ExecuteCallback execute_callback = NULL);
 
@@ -97,8 +97,8 @@ public:
    * @param n A NodeHandle to create a namespace under
    * @param name A name for the action server
    * @param execute_callback Optional callback that gets called in a separate thread whenever
-   *                   a new goal is received, allowing users to have blocking callbacks.
-   *                   Adding an execute callback also deactivates the goalCallback.
+   *                         a new goal is received, allowing users to have blocking callbacks.
+   *                         Adding an execute callback also deactivates the goalCallback.
    * @param  auto_start A boolean value that tells the ActionServer wheteher or not to start publishing as soon as it comes up. THIS SHOULD ALWAYS BE SET TO FALSE TO AVOID RACE CONDITIONS and start() should be called after construction of the server.
    */
   SimpleActionServer(ros::NodeHandle n, std::string name, ExecuteCallback execute_callback,
@@ -117,8 +117,8 @@ public:
    * @param n A NodeHandle to create a namespace under
    * @param name A name for the action server
    * @param execute_callback Optional callback that gets called in a separate thread whenever
-   *                   a new goal is received, allowing users to have blocking callbacks.
-   *                   Adding an execute callback also deactivates the goalCallback.
+   *                         a new goal is received, allowing users to have blocking callbacks.
+   *                         Adding an execute callback also deactivates the goalCallback.
    */
   ROS_DEPRECATED SimpleActionServer(ros::NodeHandle n, std::string name,
     ExecuteCallback execute_callback = NULL);
