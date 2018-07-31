56b5f3869af9e1eae9751e736f5500a24c1efcf6
Johnson Shih <jshih@microsoft.com>
2018-07-30 17:28:11 -0700

    Use portable boost::this_thread::sleep() to sleep so it can be built on Windows

--- test/destruction_guard_test.cpp
+++ test/destruction_guard_test.cpp
@@ -67,7 +67,7 @@ public:
     // Don't destruct the protectors immeadiately. Sleep for a little bit, and then destruct.
     //  This will force the main thread to have to wait in it's destruct() call
     printf("protecting thread is sleeping\n");
-    usleep(5000000);
+    boost::this_thread::sleep(boost::posix_time::microseconds(5000000));
     printf("protecting thread is exiting\n");
   }
 
