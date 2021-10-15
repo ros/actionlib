2bc96e82aaf60f2f172eab0c9107d523312fdb72
Mart Moerdijk <mart@mmoerdijk.nl>
2021-10-16 00:07:52 +0200

    Added a check to see if the transition callback is for the current goal handle (#177)
    * Added a check to see if the transition callback is for the current goal handle
    
    * Removed the logging of the goal id's as they do not always exist
    
    Co-authored-by: Mart Moerdijk <mmoerdijk@smart-robotics.nl>

--- actionlib/src/actionlib/simple_action_client.py
+++ actionlib/src/actionlib/simple_action_client.py
@@ -212,6 +212,11 @@ class SimpleActionClient:
         self.gh = None
 
     def _handle_transition(self, gh):
+
+        if gh != self.gh:
+            rospy.logerr("Got a transition callback on a goal handle that we're not tracking")
+            return
+
         comm_state = gh.get_comm_state()
 
         error_msg = "Received comm state %s when in simple state %s with SimpleActionClient in NS %s" % \
